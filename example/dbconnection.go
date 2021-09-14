bb87df19a8045c238c9486fa7eecb33c3402a307
Kimmoonsu <visitant2@naver.com>
2021-09-14 18:43:16 +0900

    add insert & select api from db

--- example/dbconnection.go
+++ example/dbconnection.go
@@ -7,20 +7,54 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-type User struct {
-	ID  string
-	PWD string
-}
-
-func main() {
-	fmt.Println("Go MySQL Tutorial")
-
-	db, err := sql.Open("mysql", "root@tcp(127.0.0.1:3306)/study")
+func insert(u User) {
+	insert, err := sqldb.db.Query("INSERT INTO User VALUES ( '" + u.ID + "', '" + u.PWD + "' )")
 
 	if err != nil {
 		panic(err.Error())
 	}
-	defer db.Close()
+	defer insert.Close()
+}
+
+func selectAll() []User {
+	results, err := sqldb.db.Query("SELECT * FROM User")
+	if err != nil {
+		panic(err.Error()) // proper error handling instead of panic in your app
+	}
+
+	user := []User{}
+
+	for results.Next() {
+		u := new(User)
+		// for each row, scan the result into our tag composite object
+		err = results.Scan(&u.ID, &u.PWD)
+		if err != nil {
+			panic(err.Error()) // proper error handling instead of panic in your app
+		}
+		user = append(user, *u)
+
+	}
+	return user
+}
+
+type DB struct {
+	db  *sql.DB
+	err error
+}
+
+var sqldb DB
+
+func connection() {
+	sqldb = DB{}
+	fmt.Println("Go MySQL Tutorial")
+
+	db, err := sql.Open("mysql", "root@tcp(127.0.0.1:3306)/study")
+	sqldb.db = db
+	sqldb.err = err
+	if err != nil {
+		panic(err.Error())
+	}
+	// defer db.Close()
 
 	// insert, err := db.Query("INSERT INTO User VALUES ( 'visitant2', '1234' )")
 
@@ -29,20 +63,8 @@ func main() {
 	// }
 	// defer insert.Close()
 
-	results, err := db.Query("SELECT * FROM User")
-	if err != nil {
-		panic(err.Error()) // proper error handling instead of panic in your app
-	}
+}
 
-	for results.Next() {
-		var user User
-		// for each row, scan the result into our tag composite object
-		err = results.Scan(&user.ID, &user.PWD)
-		if err != nil {
-			panic(err.Error()) // proper error handling instead of panic in your app
-		}
-		// and then print out the tag's Name attribute
-		fmt.Println(user.ID, " : ", user.PWD)
-
-	}
+func close() {
+	sqldb.db.Close()
 }
